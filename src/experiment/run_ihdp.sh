beb097b5fe74088a4d4a85886f11b06b2d909c23
ra237 <s_kurka@web.de>
2022-04-13 16:59:07 +0200

    minor fixes

--- src/experiment/run_ihdp.sh
+++ src/experiment/run_ihdp.sh
@@ -3,14 +3,11 @@
 options=(
     dragonnet
     tarnet
-
 )
 
 for i in ${options[@]}
 do
     echo $i
-    python3 -m experiment.ihdp_main --data_base_dir ../dat/ihdp/csv --knob $i\
+    python -m experiment.ihdp_main --data_base_dir ../dat/ihdp/csv --knob $i\
                                     --output_base_dir ../result/ihdp
-
-
 done
