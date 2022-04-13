fd636834afc519abea9d21ab9fc079de2227a5a8
ra237 <raiden237@web.de>
2022-04-13 16:37:04 +0200

    version upgrade

--- src/experiment/run_ihdp.sh
+++ src/experiment/run_ihdp.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-
 options=(
     dragonnet
     tarnet
 
 )
 
-
-
-for i in ${options[@]}; do
+for i in ${options[@]}
+do
     echo $i
-    python -m experiment.ihdp_main --data_base_dir /Users/claudiashi/ml/dragonnet/dat/ihdp/csv\
-                                 --knob $i\
-                                 --output_base_dir /Users/claudiashi//ml/dragonnet/result/ihdp\
+    python3 -m experiment.ihdp_main --data_base_dir ../dat/ihdp/csv --knob $i\
+                                    --output_base_dir ../result/ihdp
 
 
 done
